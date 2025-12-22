4f85dff12e82dbb3163492fd0051351b33b0e3d1
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-22 18:11:35 -0500

    [Auto-Publish] Index: pedal-your-way-to-fitness-the-yosuda-exercise-bike-review

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -4,10 +4,12 @@ import { post as unleash_vitality_how_zinc_50mg_transforms_your_health_journe } 
 import { post as nordictrack_treadmill_your_gateway_to_a_healthier_fitter_you } from './nordictrack-treadmill-your-gateway-to-a-healthier-fitter-you.data'
 import { post as unleash_your_grip_strengthen_hands_with_the_grip_strengthene } from './unleash-your-grip-strengthen-hands-with-the-grip-strengthene.data'
 import { post as unlock_your_strength_why_creatine_monohydrate_is_a_game_chan } from './unlock-your-strength-why-creatine-monohydrate-is-a-game-chan.data'
+import { post as pedal_your_way_to_fitness_the_yosuda_exercise_bike_review } from './pedal-your-way-to-fitness-the-yosuda-exercise-bike-review.data'
 // Blog Data Index - APPEND-ONLY System
 // 🤖 Backend appends to this file when new posts are built
 
 export const allPosts: any[] = []
+  pedal_your_way_to_fitness_the_yosuda_exercise_bike_review,
   unlock_your_strength_why_creatine_monohydrate_is_a_game_chan,
   unleash_your_grip_strengthen_hands_with_the_grip_strengthene,
   nordictrack_treadmill_your_gateway_to_a_healthier_fitter_you,
