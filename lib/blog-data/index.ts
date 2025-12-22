199a80cbb383b38a7e04be15dd6554ada3c4e9ae
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-22 15:58:40 -0500

    [Auto-Publish] Index: unlock-your-strength-the-resistance-bands-that-change-your-g

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -1,4 +1,6 @@
+import { post as unlock_your_strength_the_resistance_bands_that_change_your_g } from './unlock-your-strength-the-resistance-bands-that-change-your-g.data'
 // Blog Data Index - APPEND-ONLY System
 // 🤖 Backend appends to this file when new posts are built
 
 export const allPosts: any[] = []
+  unlock_your_strength_the_resistance_bands_that_change_your_g,
